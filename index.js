781fef38b775c9416d61ba2e9bb4adda9d0d4d47
Mionne Gooch <mionne.gooch@outlook.com>
2021-08-01 22:39:15 -0400

    Mionne Gooch Week 2 Homework VIT

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 let position = 0;
 
+
 function prevPic(){
     const slide = document.getElementById('JSslider');
     if (position < 0){
         position += 400;
-        console.log('Position is '+position+ 'px!');
+        console.log('Position is '+ position + 'px!');
     }
     slide.style.transform = `translateX(${position}px)`;
+    setTimeout(prevPic,1500);
 }
 
 function nextPic(){
     const slide = document.getElementById('JSslider');
     if (position > -2000){
         position -= 400;
-        console.log('Position is '+position+ 'px!');
+        console.log('Position is '+ position + 'px!');
     }
     slide.style.transform = `translateX(${position}px)`;
+    setTimeout(nextPic,1500);
 }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+    
+
